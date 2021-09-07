77b2e5e60a06aa1e62b4ac9560e6f3c6504b6f08
tkabeee <tkabkondo@gmail.com>
2021-09-07 21:15:34 +0900

    マップを表示

--- src/components/map-canvas.tsx
+++ src/components/map-canvas.tsx
@@ -1,3 +1,34 @@
+import { createRef, useCallback, useEffect, useRef } from 'react'
+import styled from 'styled-components'
+
+const Map = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+
 export const MapCanvas = () => {
-  return <></>
+  const mapRef = useRef<HTMLInputElement>(
+    null
+  ) as React.MutableRefObject<HTMLInputElement>
+
+  // 岡山後楽園
+  const defaultCenter = {
+    lat: 34.666624,
+    lng: 133.935692,
+  }
+
+  useEffect(() => {
+    if (mapRef && mapRef.current) {
+      const map = new google.maps.Map(mapRef.current, {
+        center: defaultCenter,
+        zoom: 10,
+      })
+    }
+  }, [])
+
+  return (
+    <>
+      <Map id="googleMap" ref={mapRef} />
+    </>
+  )
 }
