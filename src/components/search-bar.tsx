f88227f9dce8a5873887e99a460f3f318f9c6a20
tkabeee <tkabkondo@gmail.com>
2021-09-18 10:58:24 +0900

    tweet search bar component インターフェイス程度とレイアウトコーディング

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,3 +1,21 @@
-export const SearchBar = () => {
-  return <></>
+interface Props {
+  value: string
+  placeholder: string
+  onChangeText: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export const SearchBar = ({ value, placeholder, onChangeText }: Props) => {
+  return (
+    <>
+      <div>
+        <input
+          type="search"
+          value={value}
+          placeholder={placeholder}
+          onChange={(e) => onChangeText(e)}
+        />
+        <button>search</button>
+      </div>
+    </>
+  )
 }
