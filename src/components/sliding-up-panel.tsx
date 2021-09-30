3a8b22955d4ad49018bcdb8554a436083bc728ea
tkabeee <tkabkondo@gmail.com>
2021-09-30 14:49:19 +0900

    add interface

--- src/components/sliding-up-panel.tsx
+++ src/components/sliding-up-panel.tsx
@@ -5,8 +5,25 @@ import { useRef } from 'react'
 
 interface Props {
   height: number
+  draggableRage: {
+    top: number
+    bottom: number
+  }
+  snappingPoints: number[]
+  minimumVelocityThreshold: number
+  minimumDistanceThreshold: number
+  onBackButtonPress?(): void
   onDragStart?(): void
   onDragEnd?(): void
+  onBottomReached?(): void
+  allowMomentum: boolean
+  allowDragging: boolean
+  showBackdrop: boolean
+  backdropOpacity: number
+  friction: number
+  containerStyle: {}
+  backdropStyle: {}
+  children: any
 }
 
 export const SlidingUpPanel = (props: Props) => {
